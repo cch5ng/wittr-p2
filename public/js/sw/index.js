0c3a3a16786d7d423d0d24b32b226333692179f3
cch5ng <cch5ng@gmail.com>
2016-06-17 23:54:48 -0700

    chore: quiz soln

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,5 +2,11 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+	//console.log(event.request);
+
+	event.respondWith(new Response('<p class="a-winner-is-me">hi</p>', 
+	{ headers: {
+		'Content-Type': 'text/html'
+		}
+	}));
 });
