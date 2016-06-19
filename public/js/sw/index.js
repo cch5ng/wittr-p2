a1be0a9b6de2fc3adee2946989349621ccbe6913
cch5ng <cch5ng@gmail.com>
2016-06-19 14:20:53 -0700

    chore: jake soln

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -3,8 +3,14 @@ self.addEventListener('fetch', function(event) {
   // url ending in ".jpg"
   console.log('event type: ' + typeof event);
   console.log('event target: ' + event.target);
+  console.log('event request: ' + event.request);
+
+//my soln (works but is more complex than nec)
+//  if (event.request.url.match(/\S+\.jpg/g)) {
+
+//jake solution
   if (event.request.url.match(/\S+\.jpg/g)) {
-	  event.respondWith(
+  	  event.respondWith(
 	    fetch('/imgs/dr-evil.gif')
 	  );
   } else {
