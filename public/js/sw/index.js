30ad22f3c596bd1c7f2b22e3153c110d2a5a8217
cch5ng <cch5ng@gmail.com>
2016-06-19 14:11:17 -0700

    chore: soln

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,7 +1,19 @@
 self.addEventListener('fetch', function(event) {
   // TODO: only respond to requests with a
   // url ending in ".jpg"
-  event.respondWith(
-    fetch('/imgs/dr-evil.gif')
-  );
+  console.log('event type: ' + typeof event);
+  console.log('event target: ' + event.target);
+  if (event.request.url.match(/\S+\.jpg/g)) {
+	  event.respondWith(
+	    fetch('/imgs/dr-evil.gif')
+	  );
+  } else {
+  	event.respondWith(
+	    new Response('<h4>sorry but I cannot show an image</h4>', {
+	    	headers: {
+	    		'Content-Type': 'text/html'
+	    	}
+	    })
+	);
+  }
 });
