b6559741aafda9eb130ededb54a5aae042d0df60
cch5ng <cch5ng@gmail.com>
2016-06-20 16:45:53 -0700

    chore: latest instructor notes

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,9 +1,11 @@
+var staticCacheName = 'wittr-static-v2';
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
     // TODO: change the site's theme, eg swap the vars in public/scss/_theme.scss
     // Ensure at least $primary-color changes
     // TODO: change cache name to 'wittr-static-v2'
-    caches.open('wittr-static-v2').then(function(cache) {
+    caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
         '/',
         'js/main.js',
@@ -20,10 +22,14 @@ self.addEventListener('activate', function(event) {
   event.waitUntil(
     // TODO: remove the old cache
     caches.keys().then(function(cacheNames) {
+    	//use Promise.all() to make sure that all promises return
     	return Promise.all(
     		console.log('length cacheNames: ' + cacheNames.length);
+    		//instructor soln used filter and then map but same concept
+    		//he does a check to verify cache for other apps running on same origin
+    		//such as github pages
     		cacheNames.map(function(cache) {
-     			if (cache !== 'wittr-static-v2') {
+     			if (cache !== staticCacheName) {
 	     			return caches.delete(cache)
      			}
      		})
